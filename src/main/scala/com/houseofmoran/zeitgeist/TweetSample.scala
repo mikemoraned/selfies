39f990608a78dc3d169e14ff6b4b829f4b4bdc68
Mike Moran <mike@houseofmoran.com>
2015-01-03 00:24:27 +0100

    use JsonDSL implicits instead of explicit JArray etc

--- src/main/scala/com/houseofmoran/zeitgeist/TweetSample.scala
+++ src/main/scala/com/houseofmoran/zeitgeist/TweetSample.scala
@@ -1,10 +1,9 @@
 package com.houseofmoran.zeitgeist
 
-import java.io.PrintWriter
 import org.apache.spark.rdd.RDD
-import twitter4j.Status
-
 import org.json4s._
+import twitter4j.Status
+import org.json4s.JsonDSL._
 
 case class TweetSample() {
   var possibleSample : Option[Seq[Status]] = None
@@ -16,23 +15,17 @@ case class TweetSample() {
   def toJSON() : JValue = {
     possibleSample match {
       case Some(sample) => {
-        val entries =
-          for (status : Status <- sample if status.getGeoLocation() != null)
-            yield toJSON(status)
-
-        new JArray( entries.toList )
+        for (status : Status <- sample if status.getGeoLocation() != null)
+          yield toJSON(status)
       }
-      case None => new JArray(List())
+      case None => List()
     }
   }
 
   private def toJSON(status: Status) : JValue = {
-     new JObject(List(
-       ("id", JString(status.getId().toString)),
-       ("location", new JObject(List(
-         ("lat", JDouble(status.getGeoLocation.getLatitude)),
-         ("lon", JDouble(status.getGeoLocation.getLongitude))
-       )))
-     ))
+    ("id" -> status.getId().toString) ~
+    ("location" ->
+        ("lat" -> status.getGeoLocation.getLatitude) ~
+        ("lon" -> status.getGeoLocation.getLongitude))
   }
 }
