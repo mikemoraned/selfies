5852475d7af7db4e915231e843bc031683e8451f
Mike Moran <mike@houseofmoran.com>
2015-01-03 18:22:06 +0100

    merge in latest counts per geohash and show clinet-side as unique set, this time in green

--- src/main/scala/com/houseofmoran/zeitgeist/GeoHistory.scala
+++ src/main/scala/com/houseofmoran/zeitgeist/GeoHistory.scala
@@ -4,12 +4,18 @@ import org.apache.spark.rdd.RDD
 import org.json4s.JValue
 import org.json4s.JsonDSL._
 
+import scala.collection.immutable.HashMap
+
 class GeoHistory extends Snapshotable {
+  var countForGeoHash = new HashMap[String, Int]()
+
   override def toJSON(): JValue = {
-    List()
+    countForGeoHash
   }
 
   def newCounts(rdd: RDD[(String, Int)]): Unit = {
-    rdd.collect().map(println)
+    var latest = new HashMap[String, Int]()
+    latest ++= rdd.collect()
+    countForGeoHash = countForGeoHash.merged(latest)({ case ((k,v1),(_,v2)) => (k,v1+v2) })
   }
 }
